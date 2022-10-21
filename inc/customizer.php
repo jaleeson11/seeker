14d0e52d7f8a092d11d696f76fb0084ceb97e6be
jaleeson11 <66011869+jaleeson11@users.noreply.github.com>
2022-10-21 19:18:28 +0100

    Initial commit

---
+++ inc/customizer.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Seeker Theme Customizer
+ *
+ * @package Seeker
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function seeker_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+
+	if ( isset( $wp_customize->selective_refresh ) ) {
+		$wp_customize->selective_refresh->add_partial(
+			'blogname',
+			array(
+				'selector'        => '.site-title a',
+				'render_callback' => 'seeker_customize_partial_blogname',
+			)
+		);
+		$wp_customize->selective_refresh->add_partial(
+			'blogdescription',
+			array(
+				'selector'        => '.site-description',
+				'render_callback' => 'seeker_customize_partial_blogdescription',
+			)
+		);
+	}
+}
+add_action( 'customize_register', 'seeker_customize_register' );
+
+/**
+ * Render the site title for the selective refresh partial.
+ *
+ * @return void
+ */
+function seeker_customize_partial_blogname() {
+	bloginfo( 'name' );
+}
+
+/**
+ * Render the site tagline for the selective refresh partial.
+ *
+ * @return void
+ */
+function seeker_customize_partial_blogdescription() {
+	bloginfo( 'description' );
+}
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function seeker_customize_preview_js() {
+	wp_enqueue_script( 'seeker-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), SEEKER_VERSION, true );
+}
+add_action( 'customize_preview_init', 'seeker_customize_preview_js' );
